bbd02d2e3f043e60778cea4daddcf5441a3be17e
Rich Hewitt <rich.e.hewitt@gmail.com>
2024-03-15 12:49:34 +0000

    process info is now a plist of plists

--- pod.el
+++ pod.el
@@ -1,61 +1,47 @@
 (setq pod-process-plist '())
-(setq pod-timer-plist '(key timer-object))
+;; when a process is started, we also start a timer to periodically
+;; check if this started process should be subsequently stopped
+(setq pod-timer-plist '(process-key timer-object))
 
-;; 
-(defun pod-get-entry-list (key)
-  "Return the list associated with this key in the property list."
-  (plist-get pod-process-plist key))
+(defun pod-get-entry (process-key sub-key)
+  "The processes are each defined by a plist, and there is a
+ plist of processes. Here we return the entry in this plist of
+ plists associated with the keys of PROCESS-KEY and SUB-KEY.
+ Where SUB-KEY is one of :NAME, :EXE, :ARGS, :MINS or :PRED."
+  (plist-get (plist-get pod-process-plist process-key) sub-key))
 
-;; define a few functions that identify the list contents in a clearer way
-(defun pod-process-name (key)
-  "Return the 0th entry from the list associated with this KEY. This
-is a unique NAME to identify this process' details in the plist."
-  (nth 0 (pod-get-entry-list key)))
-
-(defun pod-process-exe (key)
-  "Return the 1st entry from the list associated with this KEY. This
-is a string EXE that identifies the executable/path for this
-process."  
-  (nth 1 (pod-get-entry-list key)))
-  
-(defun pod-process-args (key)
-  "Return the 2nd entry from the list associated with this KEY. This
-is the ARGUMENTS string passed to the process."
-  (nth 2 (pod-get-entry-list key)))
-
-(defun pod-process-timeout (key)
-  "Return the 3rd entry from the list associated with this KEY. This
-is the TIMEOUT passed to the process."
-  (nth 3 (pod-get-entry-list key)))
-
-(defun pod-process-p (key)
-  "Return the 3rd entry from the list associated with this KEY. This
-is a predicate function CONTINUE-P, which when false identifies
-that the process can be halted via the timer."
-  (nth 4 (pod-get-entry-list key)))
-
-(defun pod-process-stop (key)
-  "Stop previously started process if pod-continue-p(KEY) is not
-true and pod-process-name(KEY) is not nil (e.g. is running)."
+(defun pod-process-stop (process-key)
+  "Stop previously started process if pod-continue-p(PROCESS-KEY) is
+not true and pod-process-name(PROCESS-KEY) is not nil (e.g. is
+running)."
   (interactive)
-  (if (or (not (process-status (pod-process-name key))) (funcall (pod-process-p key)))
+  (if (or (not (process-status (pod-get-entry process-key :name)))
+	  (funcall (pod-get-entry process-key :pred))
+	  )
       (message "[debug] pod process not running or pod-process-p is true for this process.")    
-    (kill-process (pod-process-name key))
-    (cancel-timer (plist-get pod-timer-plist key))
-    (plist-put pod-timer-plist key nil)
+    ;; ELSE stop the process
+    (kill-process (pod-get-entry process-key :name))
+    ;; kill the timer associated with this process
+    (cancel-timer (plist-get pod-timer-plist process-key))
+    ;; remove the stored timer object assoiated with this process
+    (plist-put pod-timer-plist process-key nil)
   ))
 
-(defun pod-process-start (key)
-  "Start the process identified by KEY if it is not already running.
-Any associated output is directed to the *pod* buffer."
+(defun pod-process-start (process-key)
+  "Start the process identified by PROCESS-KEY if it is not already
+running. Any associated output is directed to the *pod* buffer."
   (interactive)
-  (if (process-status (pod-process-name key))
+  (if (process-status (pod-get-entry process-key :name))
       (message "[debug] pod process already running")
+    ;; ELSE
     ( let ((default-directory "~/"))
-      (start-process (pod-process-name key) "*pod*" (pod-process-exe key) (pod-process-args key))
+      (setq-local process-id (start-process (pod-get-entry process-key :name) "*pod*" (pod-get-entry process-key :exe) (pod-get-entry process-key :args)))
+      ;; for this process, don't ask user to kill y/n on exiting
+      ;; emacs before the process has been stopped by pod-process-stop
+      (set-process-query-on-exit-flag process-id nil)
       )
-    (setq timer-object (run-with-timer 10 (* (pod-process-timeout key) 60) 'pod-process-stop key)) 
-    (plist-put pod-timer-plist key timer-object)
+    (setq timer-object (run-with-timer 10 (* (pod-get-entry process-key :mins) 60) 'pod-process-stop process-key)) 
+    (plist-put pod-timer-plist process-key timer-object)
     ))
 
 ;; example 
